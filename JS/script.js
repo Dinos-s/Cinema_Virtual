eb6a2843d62dd9c3feabb8de154e726c30ae6a7e
Gabriel <gabriel02ramos@gmail.com>
2022-12-06 11:02:19 -0300

    projeto inicial dos filmes em VR

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+let pg2 = document.querySelector("#pg2")
+pg2.addEventListener("mouseenter", e=>{
+    location.href = "pg02.html"
+})
+
+let pg3 = document.querySelector("#pg3")
+pg3.addEventListener("mouseenter", e=>{
+    location.href = "pg03.html"
+})
+
+let pg4 = document.querySelector("#pg4")
+pg4.addEventListener("mouseenter", e=>{
+    location.href = "pg04.html"
+})
+
+let pg5 = document.querySelector('#pg5')
+pg5.addEventListener('mouseenter', e=>{
+    location.href = 'pg05.html'
+})
